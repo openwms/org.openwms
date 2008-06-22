87852adcbd4e0b953c7ba143377b79c42f3e4a42
Heiko Scherrer <scherrer@openwms.org>
2008-06-22 23:02:01 +0000

    Added value type Weight
    git-svn-id: https://openwms2005.svn.sourceforge.net/svnroot/openwms2005@38 fda5f131-174c-0410-8613-e0905860106b

---
+++ OpenWMS-common/trunk/src/org/openwms/domain/common/values/Unit.java
@@ -0,0 +1,13 @@
+/*
+ * OpenWMS, the open Warehouse Management System
+ * 
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package org.openwms.domain.common.values;
+
+public interface Unit<T> {
+
+	public abstract void convertTo(T unit);
+
+}
