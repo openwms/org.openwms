c1884e7224a5778c6e2420d6876c6dff0c02727f
Heiko Scherrer <scherrer@openwms.org>
2009-01-20 01:12:11 +0000

    Added isNew() methods
    git-svn-id: https://openwms2005.svn.sourceforge.net/svnroot/openwms2005@98 fda5f131-174c-0410-8613-e0905860106b

--- OpenWMS-common/trunk/src/org/openwms/tms/domain/order/TransportOrder.java
+++ OpenWMS-common/trunk/src/org/openwms/tms/domain/order/TransportOrder.java
@@ -30,9 +30,8 @@ import org.openwms.tms.domain.values.Problem;
  * 
  * A TransportOrder.
  * <p>
- * Is used to move <tt>TransportUnit</tt>s from an actual <tt>Location</tt>
- * to a target <tt>Location</tt>. A <tt>TransportOrder</tt> can only be
- * processed in a specific state (STARTED).
+ * Is used to move <tt>TransportUnit</tt>s from an actual <tt>Location</tt> to a target <tt>Location</tt>. A
+ * <tt>TransportOrder</tt> can only be processed in a specific state (STARTED).
  * 
  * @author <a href="heiko.scherrer@gmx.de">Heiko Scherrer</a>
  * @version $Revision: 40 $
@@ -41,329 +40,335 @@ import org.openwms.tms.domain.values.Problem;
 @Table(name = "TRANSPORT_ORDER")
 public class TransportOrder implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	public enum TRANSPORT_ORDER_STATE {
-		CREATED, INITIALIZED, STARTED, INTERRUPTED, ONFAILURE, FINISHED
+    public enum TRANSPORT_ORDER_STATE {
+	CREATED, INITIALIZED, STARTED, INTERRUPTED, ONFAILURE, FINISHED
+    }
+
+    /**
+     * Primary key.
+     */
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue
+    private Long id;
+
+    /**
+     * The <tt>TransportUnit</tt> to be moved by this <tt>TransportOrder</tt>.
+     */
+    @ManyToOne
+    @JoinColumn(name = "TRANSPORT_UNIT")
+    private TransportUnit transportUnit;
+
+    /**
+     * Date when the <tt>TransportOrder</tt> was last updated.
+     */
+    @Column(name = "DATE_UPDATED")
+    private Date dateUpdated;
+
+    /**
+     * A priority level of the <tt>TransportOrder</tt>. The lower the value the lower the priority.<br>
+     * The priority level affects the execution of the <tt>TransportOrder</tt>. A high priored order will be processed
+     * faster than lower ones.
+     */
+    @Column(name = "PRIORITY")
+    private short priority = 0;
+
+    /**
+     * Timestamp when the <tt>TransportOrder</tt> was started.
+     */
+    @Column(name = "START_DATE")
+    private Date startDate;
+
+    /**
+     * Last problem on this <tt>TransportOrder</tt>.
+     */
+    @Column(name = "PROBLEM")
+    private Problem problem;
+
+    /**
+     * Timestamp when the <tt>TransportOrder</tt> was created.
+     */
+    @Column(name = "CREATION_DATE")
+    private Date creationDate;
+
+    /**
+     * Timestamp when the <tt>TransportOrder</tt> ended.
+     */
+    @Column(name = "END_DATE")
+    private Date endDate;
+
+    /**
+     * State of this <code>TransportOrder</code>.
+     */
+    @Column(name = "STATE")
+    private TRANSPORT_ORDER_STATE state;
+
+    /**
+     * The source <tt>Location</tt> of the <tt>TransportOrder</tt>.<br>
+     * This property should be set before starting the <tt>TransportOrder</tt>.
+     */
+    @ManyToOne
+    @JoinColumn(name = "SOURCE_LOCATION")
+    private Location sourceLocation;
+
+    /**
+     * The target <tt>Location</tt> of the <tt>TransportOrder</tt>.<br>
+     * This property should be set before starting the <tt>TransportOrder</tt>.
+     */
+    @ManyToOne
+    @JoinColumn(name = "TARGET_LOCATION")
+    private Location targetLocation;
+
+    /**
+     * A <tt>LocationGroup</tt> can also set as target. When the <tt>TransportOrder</tt> will be started, a target must
+     * be set.
+     */
+    @ManyToOne
+    @JoinColumn(name = "TARGET_LOCATION_GROUP")
+    private LocationGroup targetLocationGroup;
+
+    /**
+     * Version field.
+     */
+    @Version
+    private long version;
+
+    /* ----------------------------- methods ------------------- */
+    /**
+     * Create a new <code>TransportOrder</code>.
+     */
+    public TransportOrder() {
+	this.creationDate = new Date();
+	this.state = TRANSPORT_ORDER_STATE.CREATED;
+    }
+
+    /**
+     * Returns the Primary Key.
+     * 
+     * @return id.
+     */
+    public Long getId() {
+	return this.id;
+    }
+
+    /**
+     * Check whether the instance is transient.
+     * 
+     * @return
+     */
+    public boolean isNew() {
+	return (this.id == null);
+    }
+
+    /**
+     * Get the priority level of this <tt>TransportOrder</tt>.
+     * 
+     * @return priority.
+     */
+    public short getPriority() {
+	return this.priority;
+    }
+
+    /**
+     * Set the priority level of this <tt>TransportOrder</tt>.The lower the value the lower the priority.
+     * 
+     * @param priority
+     */
+    public void setPriority(short priority) {
+	this.priority = priority;
+    }
+
+    /**
+     * Gets the date when the <tt>TransportOrder</tt> was started.
+     * 
+     * @return startDate
+     */
+    public Date getStartDate() {
+	return this.startDate;
+    }
+
+    /**
+     * Set the date when the <tt>TransportOrder</tt> was started.
+     * 
+     * @param startDate
+     */
+    private void setStartDate(Date startDate) {
+	this.startDate = startDate;
+    }
+
+    /**
+     * Get the <tt>TransportUnit</tt> belonging to this <tt>TransportOrder</tt>.
+     * 
+     * @return transportUnit.
+     */
+    public TransportUnit getTransportUnit() {
+	return this.transportUnit;
+    }
+
+    /**
+     * Set a <tt>TransportUnit</tt> to this <tt>TransportOrder</tt>.
+     * 
+     * @param transportUnit
+     */
+    public void setTransportUnit(TransportUnit transportUnit) {
+	this.transportUnit = transportUnit;
+    }
+
+    /**
+     * Get the date when this <tt>TransportOrder</tt> was created.
+     * 
+     * @return
+     */
+    public Date getCreationDate() {
+	return this.creationDate;
+    }
+
+    /**
+     * Get the state of this <tt>TransportOrder</tt>.
+     * 
+     * @return
+     */
+    public TRANSPORT_ORDER_STATE getState() {
+	return this.state;
+    }
+
+    private void validateInitializationCondition() {
+	if (transportUnit == null || (targetLocation == null && targetLocationGroup == null)) {
+	    throw new InsufficientValueException("Not all properties are set to switch transportOrder in next state");
 	}
+    }
 
-	/**
-	 * Primary key.
-	 */
-	@Id
-	@Column(name = "ID")
-	@GeneratedValue
-	private long id;
-
-	/**
-	 * The <tt>TransportUnit</tt> to be moved by this <tt>TransportOrder</tt>.
-	 */
-	@ManyToOne
-	@JoinColumn(name = "TRANSPORT_UNIT")
-	private TransportUnit transportUnit;
-
-	/**
-	 * Date when the <tt>TransportOrder</tt> was last updated.
-	 */
-	@Column(name = "DATE_UPDATED")
-	private Date dateUpdated;
-
-	/**
-	 * A priority level of the <tt>TransportOrder</tt>. The lower the value
-	 * the lower the priority.<br>
-	 * The priority level affects the execution of the <tt>TransportOrder</tt>.
-	 * A high priored order will be processed faster than lower ones.
-	 */
-	@Column(name = "PRIORITY")
-	private short priority = 0;
-
-	/**
-	 * Timestamp when the <tt>TransportOrder</tt> was started.
-	 */
-	@Column(name = "START_DATE")
-	private Date startDate;
-
-	/**
-	 * Last problem on this <tt>TransportOrder</tt>.
-	 */
-	@Column(name = "PROBLEM")
-	private Problem problem;
-
-	/**
-	 * Timestamp when the <tt>TransportOrder</tt> was created.
-	 */
-	@Column(name = "CREATION_DATE")
-	private Date creationDate;
-
-	/**
-	 * Timestamp when the <tt>TransportOrder</tt> ended.
-	 */
-	@Column(name = "END_DATE")
-	private Date endDate;
-
-	/**
-	 * State of this <code>TransportOrder</code>.
-	 */
-	@Column(name = "STATE")
-	private TRANSPORT_ORDER_STATE state;
-
-	/**
-	 * The source <tt>Location</tt> of the <tt>TransportOrder</tt>.<br>
-	 * This property should be set before starting the <tt>TransportOrder</tt>.
-	 */
-	@ManyToOne
-	@JoinColumn(name = "SOURCE_LOCATION")
-	private Location sourceLocation;
-
-	/**
-	 * The target <tt>Location</tt> of the <tt>TransportOrder</tt>.<br>
-	 * This property should be set before starting the <tt>TransportOrder</tt>.
-	 */
-	@ManyToOne
-	@JoinColumn(name = "TARGET_LOCATION")
-	private Location targetLocation;
-
-	/**
-	 * A <tt>LocationGroup</tt> can also set as target. When the
-	 * <tt>TransportOrder</tt> will be started, a target must be set.
-	 */
-	@ManyToOne
-	@JoinColumn(name = "TARGET_LOCATION_GROUP")
-	private LocationGroup targetLocationGroup;
-
-	/**
-	 * Version field.
-	 */
-	@Version
-	private long version;
-
-	/* ----------------------------- methods ------------------- */
-	/**
-	 * Create a new <code>TransportOrder</code>.
-	 */
-	public TransportOrder() {
-		this.creationDate = new Date();
-		this.state = TRANSPORT_ORDER_STATE.CREATED;
+    private void validateStateChange(TRANSPORT_ORDER_STATE newState) {
+	if (newState == null) {
+	    throw new IllegalStateException("transportState cannot be null");
 	}
-
-	/**
-	 * Returns the Primary Key.
-	 * 
-	 * @return id.
-	 */
-	public long getId() {
-		return this.id;
+	if (getState().compareTo(newState) > 0) {
+	    // Don't allow to turn back the state!
+	    throw new IllegalStateException("Turning back state of transportOrder not allowed");
 	}
-
-	/**
-	 * Get the priority level of this <tt>TransportOrder</tt>.
-	 * 
-	 * @return priority.
-	 */
-	public short getPriority() {
-		return this.priority;
+	if (getState() == TRANSPORT_ORDER_STATE.CREATED) {
+	    if (newState != TRANSPORT_ORDER_STATE.INITIALIZED) {
+		// Don't allow to except the initialization
+		throw new IllegalStateException("TransportOrder must be initialized after creation");
+	    }
+	    validateInitializationCondition();
 	}
+    }
 
-	/**
-	 * Set the priority level of this <tt>TransportOrder</tt>.The lower the
-	 * value the lower the priority.
-	 * 
-	 * @param priority
-	 */
-	public void setPriority(short priority) {
-		this.priority = priority;
+    /**
+     * Set the state of this <tt>TransportOrder</tt>.
+     * 
+     * @param newState
+     */
+    public void setState(TRANSPORT_ORDER_STATE newState) throws TransportManagementException {
+	validateStateChange(newState);
+	if (newState == TRANSPORT_ORDER_STATE.STARTED) {
+	    setStartDate(new Date());
 	}
+	state = newState;
+    }
 
-	/**
-	 * Gets the date when the <tt>TransportOrder</tt> was started.
-	 * 
-	 * @return startDate
-	 */
-	public Date getStartDate() {
-		return this.startDate;
-	}
+    /**
+     * Get the target <tt>Location</tt> of this <tt>TransportOrder</tt>.
+     * 
+     * @return targetLocation
+     */
+    public Location getTargetLocation() {
+	return this.targetLocation;
+    }
 
-	/**
-	 * Set the date when the <tt>TransportOrder</tt> was started.
-	 * 
-	 * @param startDate
-	 */
-	private void setStartDate(Date startDate) {
-		this.startDate = startDate;
-	}
+    /**
+     * Set the target <tt>Location</tt> of this <tt>TransportOrder</tt>.
+     * 
+     * @param targetLocation
+     */
+    public void setTargetLocation(Location targetLocation) {
+	this.targetLocation = targetLocation;
+    }
 
-	/**
-	 * Get the <tt>TransportUnit</tt> belonging to this
-	 * <tt>TransportOrder</tt>.
-	 * 
-	 * @return transportUnit.
-	 */
-	public TransportUnit getTransportUnit() {
-		return this.transportUnit;
-	}
+    /**
+     * Get the date when the <tt>TransportOrder</tt> was last updated.
+     * 
+     * @return the dateUpdated.
+     */
+    public Date getDateUpdated() {
+	return dateUpdated;
+    }
 
-	/**
-	 * Set a <tt>TransportUnit</tt> to this <tt>TransportOrder</tt>.
-	 * 
-	 * @param transportUnit
-	 */
-	public void setTransportUnit(TransportUnit transportUnit) {
-		this.transportUnit = transportUnit;
-	}
+    /**
+     * Get the targetLocationGroup.
+     * 
+     * @return the targetLocationGroup.
+     */
+    public LocationGroup getTargetLocationGroup() {
+	return targetLocationGroup;
+    }
 
-	/**
-	 * Get the date when this <tt>TransportOrder</tt> was created.
-	 * 
-	 * @return
-	 */
-	public Date getCreationDate() {
-		return this.creationDate;
-	}
+    /**
+     * Set the targetLocationGroup.
+     * 
+     * @param targetLocationGroup
+     *            The targetLocationGroup to set.
+     */
+    public void setTargetLocationGroup(LocationGroup targetLocationGroup) {
+	this.targetLocationGroup = targetLocationGroup;
+    }
 
-	/**
-	 * Get the state of this <tt>TransportOrder</tt>.
-	 * 
-	 * @return
-	 */
-	public TRANSPORT_ORDER_STATE getState() {
-		return this.state;
-	}
+    /**
+     * Get the last <tt>Problem</tt>.
+     * 
+     * @return the problem.
+     */
+    public Problem getProblem() {
+	return problem;
+    }
 
-	private void validateInitializationCondition() {
-		if (transportUnit == null || (targetLocation == null && targetLocationGroup == null)) {
-			throw new InsufficientValueException("Not all properties are set to switch transportOrder in next state");
-		}
-	}
+    /**
+     * Set the last <tt>Problem</tt>.
+     * 
+     * @param problem
+     *            The <tt>Problem</tt> to set.
+     */
+    public void setProblem(Problem problem) {
+	this.problem = problem;
+    }
 
-	private void validateStateChange(TRANSPORT_ORDER_STATE newState) {
-		if (newState == null) {
-			throw new IllegalStateException("transportState cannot be null");
-		}
-		if (getState().compareTo(newState) > 0) {
-			// Don't allow to turn back the state!
-			throw new IllegalStateException("Turning back state of transportOrder not allowed");
-		}
-		if (getState() == TRANSPORT_ORDER_STATE.CREATED) {
-			if (newState != TRANSPORT_ORDER_STATE.INITIALIZED) {
-				// Don't allow to except the initialization
-				throw new IllegalStateException("TransportOrder must be initialized after creation");
-			}
-			validateInitializationCondition();
-		}
-	}
+    /**
+     * Get the endDate.
+     * 
+     * @return the endDate.
+     */
+    public Date getEndDate() {
+	return endDate;
+    }
 
-	/**
-	 * Set the state of this <tt>TransportOrder</tt>.
-	 * 
-	 * @param newState
-	 */
-	public void setState(TRANSPORT_ORDER_STATE newState) throws TransportManagementException {
-		validateStateChange(newState);
-		if (newState == TRANSPORT_ORDER_STATE.STARTED) {
-			setStartDate(new Date());
-		}
-		state = newState;
-	}
+    /**
+     * Get the sourceLocation.
+     * 
+     * @return the sourceLocation.
+     */
+    public Location getSourceLocation() {
+	return sourceLocation;
+    }
 
-	/**
-	 * Get the target <tt>Location</tt> of this <tt>TransportOrder</tt>.
-	 * 
-	 * @return targetLocation
-	 */
-	public Location getTargetLocation() {
-		return this.targetLocation;
-	}
+    /**
+     * Set the sourceLocation.
+     * 
+     * @param sourceLocation
+     *            The sourceLocation to set.
+     */
+    public void setSourceLocation(Location sourceLocation) {
+	this.sourceLocation = sourceLocation;
+    }
 
-	/**
-	 * Set the target <tt>Location</tt> of this <tt>TransportOrder</tt>.
-	 * 
-	 * @param targetLocation
-	 */
-	public void setTargetLocation(Location targetLocation) {
-		this.targetLocation = targetLocation;
-	}
-
-	/**
-	 * Get the date when the <tt>TransportOrder</tt> was last updated.
-	 * 
-	 * @return the dateUpdated.
-	 */
-	public Date getDateUpdated() {
-		return dateUpdated;
-	}
-
-	/**
-	 * Get the targetLocationGroup.
-	 * 
-	 * @return the targetLocationGroup.
-	 */
-	public LocationGroup getTargetLocationGroup() {
-		return targetLocationGroup;
-	}
-
-	/**
-	 * Set the targetLocationGroup.
-	 * 
-	 * @param targetLocationGroup
-	 *            The targetLocationGroup to set.
-	 */
-	public void setTargetLocationGroup(LocationGroup targetLocationGroup) {
-		this.targetLocationGroup = targetLocationGroup;
-	}
-
-	/**
-	 * Get the last <tt>Problem</tt>.
-	 * 
-	 * @return the problem.
-	 */
-	public Problem getProblem() {
-		return problem;
-	}
-
-	/**
-	 * Set the last <tt>Problem</tt>.
-	 * 
-	 * @param problem
-	 *            The <tt>Problem</tt> to set.
-	 */
-	public void setProblem(Problem problem) {
-		this.problem = problem;
-	}
-
-	/**
-	 * Get the endDate.
-	 * 
-	 * @return the endDate.
-	 */
-	public Date getEndDate() {
-		return endDate;
-	}
-
-	/**
-	 * Get the sourceLocation.
-	 * 
-	 * @return the sourceLocation.
-	 */
-	public Location getSourceLocation() {
-		return sourceLocation;
-	}
-
-	/**
-	 * Set the sourceLocation.
-	 * 
-	 * @param sourceLocation
-	 *            The sourceLocation to set.
-	 */
-	public void setSourceLocation(Location sourceLocation) {
-		this.sourceLocation = sourceLocation;
-	}
-
-	/**
-	 * JPA optimistic locking: Returns version field.
-	 * 
-	 * @return
-	 */
-	public long getVersion() {
-		return this.version;
-	}
+    /**
+     * JPA optimistic locking: Returns version field.
+     * 
+     * @return
+     */
+    public long getVersion() {
+	return this.version;
+    }
 }
