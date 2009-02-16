aaa4914710d5124728f6ed3ab5140f7e0b6e38e1
Heiko Scherrer <scherrer@openwms.org>
2009-02-16 01:45:35 +0000

    Minor changes
    git-svn-id: https://openwms2005.svn.sourceforge.net/svnroot/openwms2005@109 fda5f131-174c-0410-8613-e0905860106b

---
+++ OpenWMS-common/branches/Spring/src/org/openwms/common/service/impl/EntityServiceImpl.java
@@ -0,0 +1,37 @@
+/*
+ * OpenWMS, the open Warehouse Management System
+ * 
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package org.openwms.common.service.impl;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.openwms.common.dao.GenericDAO;
+import org.openwms.common.service.EntityService;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * A EntityServiceImpl.
+ * 
+ * @author <a href="heiko.scherrer@gmx.de">Heiko Scherrer</a>
+ * @version $Revision: 314 $
+ */
+@Transactional
+public class EntityServiceImpl<T extends Serializable, ID extends Serializable> implements EntityService<T,ID> {
+
+    private GenericDAO<T, ID> dao;
+
+    @Required
+    public void setDao(GenericDAO<T, ID> dao) {
+	this.dao = dao;
+    }
+    
+    public List<T> findAll(Class<T> clazz) {
+	dao.setPersistentClass(clazz);
+	return dao.findAll();
+    }
+}
