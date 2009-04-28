e3642316245b6a6f93cacbe7ca87079760401476
scherrer <scherrer@fda5f131-174c-0410-8613-e0905860106b>
2009-04-28 21:30:45 +0000

    created
    git-svn-id: https://openwms2005.svn.sourceforge.net/svnroot/openwms2005@196 fda5f131-174c-0410-8613-e0905860106b

---
+++ OpenWMS-common/branches/OpenWMS-Services/src/org/openwms/common/service/LocationService.java
@@ -0,0 +1,19 @@
+/*
+ * OpenWMS, the open Warehouse Management System
+ * 
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package org.openwms.common.service;
+
+import java.io.Serializable;
+
+/**
+ * A LocationService.
+ * 
+ * @author <a href="heiko.scherrer@gmx.de">Heiko Scherrer</a>
+ * @version $Revision: 314 $
+ */
+public interface LocationService<T extends Serializable> extends EntityService<T> {
+
+}
