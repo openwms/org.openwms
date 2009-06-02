fe37dff909cf0e16dae445a8321134dfde53aaeb
Heiko Scherrer <scherrer@openwms.org>
2009-06-02 20:44:25 +0000

    Made a hack :-) to support multiple persistence units
    git-svn-id: https://openwms2005.svn.sourceforge.net/svnroot/openwms2005@305 fda5f131-174c-0410-8613-e0905860106b

---
+++ OpenWMS-common/trunk/OpenWMS-common-Spring/src/org/openwms/common/dao/CustomPersistenceUnitManager.java
@@ -0,0 +1,39 @@
+/*
+ * OpenWMS, the open Warehouse Management System
+ * 
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package org.openwms.common.dao;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.orm.jpa.persistenceunit.DefaultPersistenceUnitManager;
+import org.springframework.orm.jpa.persistenceunit.MutablePersistenceUnitInfo;
+
+/**
+ * A CustomPersistenceUnitManager.
+ * 
+ * @author <a href="heiko.scherrer@gmx.de">Heiko Scherrer</a>
+ * @version $Revision: 314 $
+ */
+public class CustomPersistenceUnitManager extends DefaultPersistenceUnitManager {
+	List<URL> url = new ArrayList<URL>();
+
+	@Override
+	protected void postProcessPersistenceUnitInfo(MutablePersistenceUnitInfo pui) {
+		super.postProcessPersistenceUnitInfo(pui);
+		pui.addJarFileUrl(pui.getPersistenceUnitRootUrl());
+
+		MutablePersistenceUnitInfo oldPui = getPersistenceUnitInfo(pui.getPersistenceUnitName());
+		if (oldPui != null) {
+			List<URL> urls = oldPui.getJarFileUrls();
+			for (URL url : urls) {
+				pui.addJarFileUrl(url);
+			}
+		}
+
+	}
+}
---
+++ OpenWMS-common/trunk/OpenWMS-common-Spring/src/org/openwms/common/dao/CustomPersistenceUnitPostProcessor.java
@@ -0,0 +1,84 @@
+/*
+ * OpenWMS, the open Warehouse Management System
+ * 
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package org.openwms.common.dao;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.orm.jpa.persistenceunit.MutablePersistenceUnitInfo;
+import org.springframework.orm.jpa.persistenceunit.PersistenceUnitPostProcessor;
+import org.springframework.util.ResourceUtils;
+
+/**
+ * A CustomPersistenceUnitPostProcessor.
+ * 
+ * @author <a href="heiko.scherrer@gmx.de">Heiko Scherrer</a>
+ * @version $Revision: 314 $
+ */
+public class CustomPersistenceUnitPostProcessor implements PersistenceUnitPostProcessor {
+
+	/**
+	 * Default location of the persistence jar file: "classpath*:OpenWMS*".
+	 */
+	public final static String DEFAULT_PERSISTENCE_JAR_FILES = "classpath*:OpenWMS*";
+
+	/** Location of persistence jar file(s) */
+	private String[] persistenceJarFiles = new String[] { DEFAULT_PERSISTENCE_JAR_FILES };
+
+	private ResourcePatternResolver resourcePatternResolver = new PathMatchingResourcePatternResolver();
+
+	private List<Resource> jarFiles = new ArrayList<Resource>();
+
+	private void resolveJarFiles() {
+		try {
+			for (int i = 0; i < persistenceJarFiles.length; i++) {
+				System.out.println("--"+ResourceUtils.getURL(persistenceJarFiles[i]));
+				Resource[] resources = this.resourcePatternResolver.getResources(persistenceJarFiles[i]);
+				for (Resource resource : resources) {
+					jarFiles.add(resource);
+				}
+			}
+		}
+		catch (IOException ex) {
+			throw new IllegalArgumentException("Cannot load persistence jar file from ", ex);
+		}
+	}
+
+	/**
+	 * Specify multiple locations of <code>persistence.xml</code> files to load. These can be specified as Spring
+	 * resource locations and/or location patterns.
+	 * <p>
+	 * Default is "classpath*:META-INF/persistence.xml".
+	 * 
+	 * @param persistenceXmlLocations
+	 *            an array of Spring resource Strings identifying the location of the <code>persistence.xml</code> files
+	 *            to read
+	 */
+	public void setPersistenceJarFiles(String[] persistenceJarFiles) {
+		this.persistenceJarFiles = persistenceJarFiles;
+	}
+
+	public void postProcessPersistenceUnitInfo(MutablePersistenceUnitInfo pui) {
+		if (jarFiles.size() == 0) {
+			resolveJarFiles();
+		}
+		try {
+			for (Resource resource : jarFiles) {
+				pui.addJarFileUrl(resource.getURL());
+			}
+		}
+		catch (IOException e) {
+			throw new IllegalArgumentException("Cannot parse persistence unit from " + pui.getPersistenceUnitName(), e);
+		}
+	}
+
+}
