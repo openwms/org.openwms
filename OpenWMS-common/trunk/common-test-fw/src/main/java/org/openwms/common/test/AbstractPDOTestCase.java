6a492bcde71c6e6be557b2c46399188189b65d47
Heiko Scherrer <scherrer@openwms.org>
2009-09-07 22:40:35 +0000

    Removed generated code files
    git-svn-id: https://openwms2005.svn.sourceforge.net/svnroot/openwms2005@398 fda5f131-174c-0410-8613-e0905860106b

--- OpenWMS-common/trunk/common-test-fw/src/main/java/org/openwms/common/test/AbstractPDOTestCase.java
+++ OpenWMS-common/trunk/common-test-fw/src/main/java/org/openwms/common/test/AbstractPDOTestCase.java
@@ -4,7 +4,7 @@
  * Distributable under LGPL license.
  * See terms of license at gnu.org.
  */
-package org.openwms.common.domain.helper;
+package org.openwms.common.test;
 
 import static org.junit.Assert.fail;
 
