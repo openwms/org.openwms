ff3f695d1c8db21c91bf761e0d3443ad2abb6135
scherrer <scherrer@fda5f131-174c-0410-8613-e0905860106b>
2009-11-03 22:38:40 +0000

    2886764 Migration of all modules to OSGi bundles
    git-svn-id: https://openwms2005.svn.sourceforge.net/svnroot/openwms2005@452 fda5f131-174c-0410-8613-e0905860106b

--- OpenWMS-common/trunk/org.openwms.common.framework.test/src/test/java/org/openwms/common/test/AbstractJpaSpringContextTests.java
+++ OpenWMS-common/trunk/org.openwms.common.framework.test/src/test/java/org/openwms/common/test/AbstractJpaSpringContextTests.java
@@ -6,8 +6,14 @@
  */
 package org.openwms.common.test;
 
-import org.springframework.stereotype.Component;
-import org.springframework.test.jpa.AbstractJpaTests;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
 /**
  * A AbstractJpaSpringContextTests.
@@ -15,20 +21,11 @@ import org.springframework.test.jpa.AbstractJpaTests;
  * @author <a href="mailto:openwms@googlemail.com">Heiko Scherrer</a>
  * @version $Revision: 314 $
  */
-@Component
-public abstract class AbstractJpaSpringContextTests extends AbstractJpaTests {
-
-	@Override
-	protected boolean shouldUseShadowLoader() {
-		return false;
-	}
-
-	@Override
-	protected String[] getConfigLocations() {
-		return new String[] {
-				"classpath*:/**/Test-Infrastructure-context.xml",
-				"classpath*:**/*" + this.getClass().getPackage().getName().replace('.', '/') + "/"
-						+ this.getClass().getSimpleName() + "-context.xml" };
-	}
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath*:/**/Test-Infrastructure-context.xml" })
+@TestExecutionListeners(value = DependencyInjectionTestExecutionListener.class)
+public abstract class AbstractJpaSpringContextTests extends AbstractTransactionalJUnit4SpringContextTests {
+	
+	protected final static Log log = LogFactory.getLog(AbstractJpaSpringContextTests.class); 
 
 }
