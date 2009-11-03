c4257d41b99c7efb05e8b5a4982854d2898b83ea
Heiko Scherrer <scherrer@openwms.org>
2009-11-03 22:30:24 +0000

    2886764 Migration of all modules to OSGi bundles
    git-svn-id: https://openwms2005.svn.sourceforge.net/svnroot/openwms2005@445 fda5f131-174c-0410-8613-e0905860106b

---
+++ OpenWMS-common/trunk/org.openwms.common.integration.jpa/src/main/java/org/openwms/common/integration/jpa/LocationGroupDaoImpl.java
@@ -0,0 +1,37 @@
+/*
+ * OpenWMS, the open Warehouse Management System
+ * 
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package org.openwms.common.integration.jpa;
+
+import java.util.Date;
+
+import org.openwms.common.domain.LocationGroup;
+import org.openwms.common.integration.LocationGroupDao;
+
+/**
+ * A LocationGroupDaoImpl.
+ * 
+ * @author <a href="mailto:openwms@googlemail.com">Heiko Scherrer</a>
+ * @version $Revision: 314 $
+ */
+public class LocationGroupDaoImpl extends AbstractGenericJpaDao<LocationGroup, Long> implements LocationGroupDao {
+
+	@Override
+	protected String getFindAllQuery() {
+		return LocationGroupDao.NQ_FIND_ALL;
+	}
+
+	@Override
+	protected String getFindByUniqueIdQuery() {
+		return LocationGroupDao.NQ_FIND_BY_NAME;
+	}
+
+	@Override
+	protected void beforeUpdate(LocationGroup entity) {
+		entity.setLastUpdated(new Date());
+	}
+
+}
---
+++ OpenWMS-common/trunk/org.openwms.common.integration.jpa/src/main/java/org/openwms/common/integration/jpa/TransportUnitTypeDaoImpl.java
@@ -0,0 +1,31 @@
+/*
+ * OpenWMS, the open Warehouse Management System
+ * 
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package org.openwms.common.integration.jpa;
+
+import org.openwms.common.domain.TransportUnitType;
+import org.openwms.common.integration.TransportUnitTypeDao;
+
+/**
+ * A TransportUnitTypeDaoImpl.
+ * 
+ * @author <a href="mailto:openwms@googlemail.com">Heiko Scherrer</a>
+ * @version $Revision: 314 $
+ */
+public class TransportUnitTypeDaoImpl extends AbstractGenericJpaDao<TransportUnitType, String> implements
+		TransportUnitTypeDao {
+
+	@Override
+	protected String getFindAllQuery() {
+		return TransportUnitTypeDao.NQ_FIND_ALL;
+	}
+
+	@Override
+	protected String getFindByUniqueIdQuery() {
+		return TransportUnitTypeDao.NQ_FIND_BY_NAME;
+	}
+
+}
