7c7d4ff0e6782bcda8612c9b656f3dd6522bc328
Heiko Scherrer <scherrer@openwms.org>
2010-02-11 01:07:00 +0000

    Updated configuration files, namespaces and file headers
    git-svn-id: https://openwms2005.svn.sourceforge.net/svnroot/openwms2005@615 fda5f131-174c-0410-8613-e0905860106b

--- org.openwms/trunk/source/org.openwms.common.service.api/src/main/java/org/openwms/common/service/management/UserService.java
+++ org.openwms/trunk/source/org.openwms.common.service.api/src/main/java/org/openwms/common/service/management/UserService.java
@@ -1,60 +1,79 @@
 /*
- * OpenWMS, the open Warehouse Management System
- * 
- * Distributable under LGPL license.
- * See terms of license at gnu.org.
+ * openwms.org, the Open Warehouse Management System.
+ *
+ * This file is part of openwms.org.
+ *
+ * openwms.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as 
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * openwms.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software. If not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
 package org.openwms.common.service.management;
 
 import java.io.Serializable;
 
+import org.openwms.common.domain.system.usermanagement.User;
 import org.openwms.common.service.EntityService;
 
 /**
- * A UserService.
+ * An UserService.
+ * <p>
+ * Extends the {@link EntityService} interface about some useful methods
+ * regarding the general handling with {@link User}s.
+ * </p>
  * 
  * @author <a href="mailto:openwms@googlemail.com">Heiko Scherrer</a>
  * @version $Revision: 877 $
+ * @since 0.1
+ * @see EntityService
  */
 public interface UserService<T extends Serializable> extends EntityService<T> {
 
     /**
-     * Call this method to store an image of an user.
+     * Call this method to store an image of an {@link User}.
      * 
      * @param username
      *            - Username of the User
      * @param image
      *            - Image as byte[]
-     * @return - true, if operation successful - false, if not successful
+     * @return - true, if operation successful<br>
+     *         - false, if not
      */
     boolean uploadImageFile(String username, byte[] image);
 
     /**
-     * 
-     * Return an transient User entity class, serving as a template
+     * Return an transient {@link User} Entity class, serving as a template.
      * 
      * @param username
-     *            - Username of the User
-     * @return - An empty template object of an User entity
+     *            - Username of the {@link User}
+     * @return - An empty template {@link User} instance
      */
     T getTemplate(String username);
 
     /**
-     * 
-     * Save the given User entity class or persist it when it is transient.
+     * Save the given {@link User} Entity or persist it when it is transient.
      * 
      * @param user
-     *            - User entity instance
-     * @return - Saved User entity instance
+     *            - {@link User} Entity to persist
+     * @return - Saved {@link User} Entity instance
      */
     T save(T user);
 
     /**
-     * 
-     * Remove the User entity from the persistence.
+     * Remove {@link User} Entity from the persistence storage.
      * 
      * @param user
-     *            - User entity instance
+     *            - {@link User} Entity to be removed
      */
     void remove(T user);
 
