b12bf9520d327f79b09d5be5a396cee3ab4179f3
scherrer <scherrer@fda5f131-174c-0410-8613-e0905860106b>
2010-02-16 01:12:26 +0000

    Updated configuration files, namespaces and file headers
    git-svn-id: https://openwms2005.svn.sourceforge.net/svnroot/openwms2005@629 fda5f131-174c-0410-8613-e0905860106b

--- org.openwms/trunk/source/org.openwms.common.service.spring/src/main/java/org/openwms/common/service/spring/LocationGroupServiceImpl.java
+++ org.openwms/trunk/source/org.openwms.common.service.spring/src/main/java/org/openwms/common/service/spring/LocationGroupServiceImpl.java
@@ -1,8 +1,22 @@
 /*
- * OpenWMS, the open Warehouse Management System
- * 
- * Distributable under LGPL license.
- * See terms of license at gnu.org.
+ * openwms.org, the Open Warehouse Management System.
+ *
+ * This file is part of openwms.org.
+ *
+ * openwms.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as 
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * openwms.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software. If not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
 package org.openwms.common.service.spring;
 
@@ -23,77 +37,96 @@ import org.springframework.transaction.annotation.Transactional;
  * 
  * @author <a href="mailto:openwms@googlemail.com">Heiko Scherrer</a>
  * @version $Revision: 314 $
+ * @since 0.1
  */
 @Transactional
 @Service
 public class LocationGroupServiceImpl extends EntityServiceImpl<LocationGroup, Long> implements
-		LocationGroupService<LocationGroup> {
+        LocationGroupService<LocationGroup> {
 
-	public void changeGroupState(LocationGroup locationGroup) throws ServiceException {
-		logger.debug("change group state called");
-		if (null != locationGroup && locationGroup.getParent() != null
-				&& locationGroup.getParent().getGroupStateIn() == STATE.NOT_AVAILABLE
-				&& locationGroup.getGroupStateIn() == STATE.AVAILABLE) {
-			throw new ServiceException("Not allowed to change GroupStateIn, parent locationGroup is not available");
-		}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void changeGroupState(LocationGroup locationGroup) throws ServiceException {
+        logger.debug("change group state called");
+        if (null != locationGroup && locationGroup.getParent() != null
+                && locationGroup.getParent().getGroupStateIn() == STATE.NOT_AVAILABLE
+                && locationGroup.getGroupStateIn() == STATE.AVAILABLE) {
+            throw new ServiceException("Not allowed to change GroupStateIn, parent locationGroup is not available");
+        }
 
-		// Attach
-		locationGroup = dao.save(locationGroup);
-		Set<LocationGroup> childs = locationGroup.getLocationGroups();
-		for (LocationGroup child : childs) {
-			child.setGroupStateIn(locationGroup.getGroupStateIn());
-			child.setGroupStateOut(locationGroup.getGroupStateOut());
-			changeGroupState(child);
-		}
-	}
+        // Attach
+        locationGroup = dao.save(locationGroup);
+        Set<LocationGroup> childs = locationGroup.getLocationGroups();
+        for (LocationGroup child : childs) {
+            child.setGroupStateIn(locationGroup.getGroupStateIn());
+            child.setGroupStateOut(locationGroup.getGroupStateOut());
+            changeGroupState(child);
+        }
+    }
 
-	public LocationGroup save(LocationGroup locationGroup) {
-		logger.debug("save LG called");
-		return dao.save(locationGroup);
-	}
+    /**
+     * FIXME [scherrer] Comment this
+     * 
+     * @param locationGroup
+     * @return
+     */
+    public LocationGroup save(LocationGroup locationGroup) {
+        logger.debug("save LG called");
+        return dao.save(locationGroup);
+    }
 
-	public TreeNode<LocationGroup> getLocationGroupsAsTree() {
-		logger.debug("getLocationGroupsAsTree called");
-		TreeNode<LocationGroup> tree = createTree(new TreeNodeImpl<LocationGroup>(), getLocationGroupsAsList());
-		return tree;
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public TreeNode<LocationGroup> getLocationGroupsAsTree() {
+        logger.debug("getLocationGroupsAsTree called");
+        TreeNode<LocationGroup> tree = createTree(new TreeNodeImpl<LocationGroup>(), getLocationGroupsAsList());
+        return tree;
+    }
 
-	public List<LocationGroup> getLocationGroupsAsList() {
-		logger.debug("getLocationGroupsAsList called");
-		List<LocationGroup> locationGroups = dao.findAll();
-		return locationGroups;
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<LocationGroup> getLocationGroupsAsList() {
+        logger.debug("getLocationGroupsAsList called");
+        List<LocationGroup> locationGroups = dao.findAll();
+        return locationGroups;
+    }
 
-	private TreeNode<LocationGroup> createTree(TreeNode<LocationGroup> root, List<LocationGroup> locationGroups) {
-		for (LocationGroup l : locationGroups) {
-			searchForNode(l, root);
-		}
-		return root;
-	}
+    private TreeNode<LocationGroup> createTree(TreeNode<LocationGroup> root, List<LocationGroup> locationGroups) {
+        for (LocationGroup l : locationGroups) {
+            searchForNode(l, root);
+        }
+        return root;
+    }
 
-	private TreeNode<LocationGroup> searchForNode(LocationGroup lg, TreeNode<LocationGroup> root) {
-		TreeNode<LocationGroup> node;
-		if (lg.getParent() == null) {
-			node = root.getChild(lg);
-			if (node == null) {
-				TreeNode<LocationGroup> n1 = new TreeNodeImpl<LocationGroup>();
-				n1.setData(lg);
-				n1.setParent(root);
-				root.addChild(n1.getData(), n1);
-				return n1;
-			}
-			return node;
-		} else {
-			node = searchForNode(lg.getParent(), root);
-			TreeNode<LocationGroup> child = node.getChild(lg);
-			if (child == null) {
-				child = new TreeNodeImpl<LocationGroup>();
-				child.setData(lg);
-				child.setParent(node);
-				node.addChild(lg, child);
-			}
-			return child;
-		}
-	}
+    private TreeNode<LocationGroup> searchForNode(LocationGroup lg, TreeNode<LocationGroup> root) {
+        TreeNode<LocationGroup> node;
+        if (lg.getParent() == null) {
+            node = root.getChild(lg);
+            if (node == null) {
+                TreeNode<LocationGroup> n1 = new TreeNodeImpl<LocationGroup>();
+                n1.setData(lg);
+                n1.setParent(root);
+                root.addChild(n1.getData(), n1);
+                return n1;
+            }
+            return node;
+        } else {
+            node = searchForNode(lg.getParent(), root);
+            TreeNode<LocationGroup> child = node.getChild(lg);
+            if (child == null) {
+                child = new TreeNodeImpl<LocationGroup>();
+                child.setData(lg);
+                child.setParent(node);
+                node.addChild(lg, child);
+            }
+            return child;
+        }
+    }
 
 }
