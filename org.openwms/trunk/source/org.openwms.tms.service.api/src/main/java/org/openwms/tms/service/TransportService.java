5afbd34531cf608d66bdde5c2c94bb2b837a1cd1
Heiko Scherrer <scherrer@openwms.org>
2010-02-23 00:42:58 +0000

    Renamed service class
    git-svn-id: https://openwms2005.svn.sourceforge.net/svnroot/openwms2005@660 fda5f131-174c-0410-8613-e0905860106b

--- org.openwms/trunk/source/org.openwms.tms.service.api/src/main/java/org/openwms/tms/service/TransportService.java
+++ org.openwms/trunk/source/org.openwms.tms.service.api/src/main/java/org/openwms/tms/service/TransportService.java
@@ -1,50 +1,51 @@
 /*
- * OpenWMS, the open Warehouse Management System
- * 
- * Distributable under LGPL license.
- * See terms of license at gnu.org.
+ * openwms.org, the Open Warehouse Management System.
+ *
+ * This file is part of openwms.org.
+ *
+ * openwms.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as 
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * openwms.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software. If not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
 package org.openwms.tms.service;
 
 import org.openwms.common.domain.LocationGroup;
-import org.openwms.common.domain.LocationPK;
 import org.openwms.common.domain.TransportUnit;
-import org.openwms.common.domain.TransportUnitType;
-import org.openwms.common.domain.values.Barcode;
-import org.openwms.common.service.exception.ServiceException;
+import org.openwms.common.service.EntityService;
 
 /**
- * 
  * A TransportService.
+ * <p>
+ * Extends the {@link EntityService} interface about some useful methods
+ * regarding the general handling with {@link TransportUnit}s.
+ * </p>
  * 
  * @author <a href="mailto:openwms@googlemail.com">Heiko Scherrer</a>
  * @version $Revision: 877 $
+ * @since 0.1
+ * @see EntityService
  */
-public interface TransportService {
+public interface TransportService extends EntityService<TransportUnit> {
 
     /**
-     * Create a new <tt>TransportUnit</tt> with the type
-     * <tt>TransportUnitType</tt> on an initial <tt>Location</tt>.
-     * 
-     * @param barcode
-     * @param actualLocation
-     * @return
-     */
-    TransportUnit createTransportUnit(Barcode barcode, TransportUnitType transportUnitType, LocationPK actualLocation);
-
-    /**
-     * Moves a <tt>TransportUnit</tt> identified by its <tt>Barcode</tt> to the
-     * given actual <tt>Location</tt> identified by the <tt>LocationPK</tt>.
-     * 
-     * @param barcode
-     * @param locationPk
-     */
-    void moveTransportUnit(Barcode barcode, LocationPK locationPk) throws ServiceException;
-
-    /**
+     * Returns the actual number of active transports that have the
+     * <tt>locationGroup</tt> as target {@link LocationGroup}.
      * 
      * @param locationGroup
-     * @return
+     *            - {@link LocationGroup} to count all active transports for
+     * @return - Number of all active transports that are on the way to this
+     *         {@link LocationGroup}
      */
     int getTransportsToLocationGroup(LocationGroup locationGroup);
 
