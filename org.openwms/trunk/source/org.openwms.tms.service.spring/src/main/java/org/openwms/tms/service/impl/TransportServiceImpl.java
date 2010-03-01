3e1668695a51000d6492c484cb69a3b8cfdbc255
scherrer <scherrer@fda5f131-174c-0410-8613-e0905860106b>
2010-03-01 21:36:12 +0000

    Modified headers and cleaned up preparing for milestone release
    git-svn-id: https://openwms2005.svn.sourceforge.net/svnroot/openwms2005@683 fda5f131-174c-0410-8613-e0905860106b

--- org.openwms/trunk/source/org.openwms.tms.service.spring/src/main/java/org/openwms/tms/service/impl/TransportServiceImpl.java
+++ org.openwms/trunk/source/org.openwms.tms.service.spring/src/main/java/org/openwms/tms/service/impl/TransportServiceImpl.java
@@ -1,8 +1,22 @@
 /*
- * OpenWMS, the open Warehouse Management System
- * 
- * Distributable under LGPL license.
- * See terms of license at gnu.org.
+ * openwms.org, the Open Warehouse Management System.
+ *
+ * This file is part of openwms.org.
+ *
+ * openwms.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as 
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * openwms.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software. If not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
 package org.openwms.tms.service.impl;
 
@@ -20,7 +34,8 @@ import org.springframework.stereotype.Service;
  * A TransportService.
  * 
  * @author <a href="mailto:openwms@googlemail.com">Heiko Scherrer</a>
- * @version $Revision: 314 $
+ * @version $Revision$
+ * @since 0.1
  */
 @Service
 public class TransportServiceImpl extends EntityServiceImpl<TransportUnit, Long> implements TransportOrderService {
