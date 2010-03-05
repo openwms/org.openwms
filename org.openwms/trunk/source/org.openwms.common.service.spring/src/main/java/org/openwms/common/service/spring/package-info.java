703f45aefff67d5a56324e54bcc5a692ac64e096
Heiko Scherrer <scherrer@openwms.org>
2010-03-05 22:03:35 +0000

    Modified headers and cleaned up preparing for milestone release
    git-svn-id: https://openwms2005.svn.sourceforge.net/svnroot/openwms2005@719 fda5f131-174c-0410-8613-e0905860106b

--- org.openwms/trunk/source/org.openwms.common.service.spring/src/main/java/org/openwms/common/service/spring/package-info.java
+++ org.openwms/trunk/source/org.openwms.common.service.spring/src/main/java/org/openwms/common/service/spring/package-info.java
@@ -19,6 +19,6 @@
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
 /**
- * This package provides business logic to operate on common classes.
+ * This package provides common used business logic.
  */
 package org.openwms.common.service.spring;
