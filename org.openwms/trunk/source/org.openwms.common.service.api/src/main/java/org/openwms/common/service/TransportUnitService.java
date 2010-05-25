4ccca9d39833a613e6d4d72170518b61ef15e074
russelltina <russelltina@fda5f131-174c-0410-8613-e0905860106b>
2010-05-25 19:11:17 +0000

    Need a service to read and update the rules
    git-svn-id: https://openwms2005.svn.sourceforge.net/svnroot/openwms2005@846 fda5f131-174c-0410-8613-e0905860106b

--- org.openwms/trunk/source/org.openwms.common.service.api/src/main/java/org/openwms/common/service/TransportUnitService.java
+++ org.openwms/trunk/source/org.openwms.common.service.api/src/main/java/org/openwms/common/service/TransportUnitService.java
@@ -40,87 +40,112 @@ import org.openwms.common.domain.values.Barcode;
  * @since 0.1
  * @see org.openwms.common.service.EntityService
  */
-public interface TransportUnitService<T extends TransportUnit> extends
-		EntityService<TransportUnit> {
+public interface TransportUnitService<T extends TransportUnit> extends EntityService<TransportUnit> {
 
-	/**
-	 * Create a new {@link TransportUnit} with the type
-	 * {@link TransportUnitType} placed on an initial <code>Location</code>.
-	 * The new {@link TransportUnit} has the given {@link Barcode} as
-	 * identifier.
-	 * 
-	 * @param barcode
-	 *            {@link Barcode} of the new {@link TransportUnit}
-	 * @param transportUnitType
-	 *            The type of the new {@link TransportUnit}
-	 * @param actualLocation
-	 *            The <code>Location</code> where the {@link TransportUnit} is
-	 *            placed on
-	 * @return The new created {@link TransportUnit} instance
-	 */
-	T createTransportUnit(Barcode barcode, TransportUnitType transportUnitType,
-			LocationPK actualLocation);
+    /**
+     * Create a new {@link TransportUnit} with the type
+     * {@link TransportUnitType} placed on an initial <code>Location</code>. The
+     * new {@link TransportUnit} has the given {@link Barcode} as identifier.
+     * 
+     * @param barcode
+     *            {@link Barcode} of the new {@link TransportUnit}
+     * @param transportUnitType
+     *            The type of the new {@link TransportUnit}
+     * @param actualLocation
+     *            The <code>Location</code> where the {@link TransportUnit} is
+     *            placed on
+     * @return The new created {@link TransportUnit} instance
+     */
+    T createTransportUnit(Barcode barcode, TransportUnitType transportUnitType, LocationPK actualLocation);
 
-	/**
-	 * Move a {@link TransportUnit} identified by its {@link Barcode} to the
-	 * given target <code>Location</code> identified by the {@link LocationPK}.
-	 * 
-	 * @param barcode
-	 *            {@link Barcode} of the {@link TransportUnit} to move
-	 * @param targetLocationPK
-	 *            Unique identifier of the target <code>Location</code>
-	 */
-	void moveTransportUnit(Barcode barcode, LocationPK targetLocationPK);
+    /**
+     * Move a {@link TransportUnit} identified by its {@link Barcode} to the
+     * given target <code>Location</code> identified by the {@link LocationPK}.
+     * 
+     * @param barcode
+     *            {@link Barcode} of the {@link TransportUnit} to move
+     * @param targetLocationPK
+     *            Unique identifier of the target <code>Location</code>
+     */
+    void moveTransportUnit(Barcode barcode, LocationPK targetLocationPK);
 
-	/**
-	 * Delete already persisted {@link TransportUnit}s from the persistence
-	 * storage. It is not allowed in all cases to delete a {@link TransportUnit},
-	 * potentially an active TransportOrder exists or Inventory is still linked
-	 * with the <tt>transportUnit</tt>.
-	 * 
-	 * @param transportUnits
-	 *            The collection of {@link TransportUnit}s to delete
-	 */
-	void deleteTransportUnits(List<T> transportUnits);
+    /**
+     * Delete already persisted {@link TransportUnit}s from the persistence
+     * storage. It is not allowed in all cases to delete a {@link TransportUnit}
+     * , potentially an active TransportOrder exists or Inventory is still
+     * linked with the <tt>transportUnit</tt>.
+     * 
+     * @param transportUnits
+     *            The collection of {@link TransportUnit}s to delete
+     */
+    void deleteTransportUnits(List<T> transportUnits);
 
-	/**
-	 * Returns an unsorted List of all {@link TransportUnit}s.
-	 * 
-	 * @return A List of all {@link TransportUnit}s
-	 */
-	List<T> getAllTransportUnits();
+    /**
+     * Returns an unsorted List of all {@link TransportUnit}s.
+     * 
+     * @return A List of all {@link TransportUnit}s
+     */
+    List<T> getAllTransportUnits();
 
-	/**
-	 * Returns an unsorted List of all {@link TransportUnitType}s.
-	 * 
-	 * @return A list of all {@link TransportUnitType}s
-	 */
-	List<TransportUnitType> getAllTransportUnitTypes();
-	
-	/**
-	 * Create a new {@link TransportUnitType}.
-	 * 
-	 * @param transportUnitType The type to be created
-	 * @return The new created {@link TransportUnitType} instance.
-	 */
-	TransportUnitType createTransportUnitType(TransportUnitType transportUnitType);
+    /**
+     * Returns an unsorted List of all {@link TransportUnitType}s.
+     * 
+     * @return A list of all {@link TransportUnitType}s
+     */
+    List<TransportUnitType> getAllTransportUnitTypes();
 
-	/**
-	 * Delete already persisted {@link TransportUnitType} instances.
-	 * 
-	 * @param transportUnitType A list of all instances to be deleted.
-	 */
-	void deleteTransportUnitTypes(List<TransportUnitType> transportUnitType);
-	
-	/**
-	 * Save an already persisted instance of {@link TransportUnitType}.
-	 * 
-	 * @param transportUnitType The instance to be updated
-	 * @return The updated instance
-	 */
-	TransportUnitType saveTransportUnitType(TransportUnitType transportUnitType);
-	
-	TransportUnitType updateRules(String type, List<LocationType> newAssigned, List<LocationType> newNotAssigned);
-	
-	List<Rule> loadRules(String transportUnitType);
+    /**
+     * Create a new {@link TransportUnitType}.
+     * 
+     * @param transportUnitType
+     *            The type to be created
+     * @return The new created {@link TransportUnitType} instance.
+     */
+    TransportUnitType createTransportUnitType(TransportUnitType transportUnitType);
+
+    /**
+     * Delete already persisted {@link TransportUnitType} instances.
+     * 
+     * @param transportUnitType
+     *            A list of all instances to be deleted.
+     */
+    void deleteTransportUnitTypes(List<TransportUnitType> transportUnitType);
+
+    /**
+     * Save an already persisted instance of {@link TransportUnitType}.
+     * 
+     * @param transportUnitType
+     *            The instance to be updated
+     * @return The updated instance
+     */
+    TransportUnitType saveTransportUnitType(TransportUnitType transportUnitType);
+
+    /**
+     * Update the List of {@link org.openwms.common.domain.TypePlacingRule}s for
+     * the given {@link TransportUnitType} type.
+     * 
+     * @param type
+     *            The {@link TransportUnitType} to update.
+     * @param newAssigned
+     *            A new List of {@link LocationType}s that are allowed for the
+     *            {@link TransportUnitType}.
+     * @param newNotAssigned
+     *            A List of {@link LocationType}s. All
+     *            {@link org.openwms.common.domain.TypePlacingRule}s will be
+     *            removed which have one of this {@link LocationType}s and the
+     *            requested {@link TransportUnitType} type.
+     * @return The updated {@link TransportUnitType}.
+     */
+    TransportUnitType updateRules(String type, List<LocationType> newAssigned, List<LocationType> newNotAssigned);
+
+    /**
+     * Return a List of all {@link org.openwms.common.domain.Rule}s that belong
+     * to this {@link TransportUnitType} type.
+     * 
+     * @param transportUnitType
+     *            The {@link TransportUnitType} to search for.
+     * @return The requested List or <code>null</code> if no {@link Rule} was
+     *         found.
+     */
+    List<Rule> loadRules(String transportUnitType);
 }
