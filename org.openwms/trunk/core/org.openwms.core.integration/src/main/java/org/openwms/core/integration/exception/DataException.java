2737d190696220240ef8e62528096a4dbebb0728
scherrer <scherrer@fda5f131-174c-0410-8613-e0905860106b>
2010-12-13 22:54:17 +0000

    reorganized
    git-svn-id: https://openwms2005.svn.sourceforge.net/svnroot/openwms2005@1110 fda5f131-174c-0410-8613-e0905860106b

--- org.openwms/trunk/core/org.openwms.core.integration/src/main/java/org/openwms/core/integration/exception/DataException.java
+++ org.openwms/trunk/core/org.openwms.core.integration/src/main/java/org/openwms/core/integration/exception/DataException.java
@@ -18,7 +18,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.openwms.common.integration.exception;
+package org.openwms.core.integration.exception;
 
 /**
  * A DataException is a general type of DAO exception.
