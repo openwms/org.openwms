e3e52d09e905cd97ca96dd085811c00c60b3d25d
Heiko Scherrer <scherrer@openwms.org>
2010-12-19 22:38:44 +0000

    reorganized
    git-svn-id: https://openwms2005.svn.sourceforge.net/svnroot/openwms2005@1173 fda5f131-174c-0410-8613-e0905860106b

--- org.openwms/trunk/core/org.openwms.core.service.api/src/main/java/org/openwms/core/service/listener/OnRemovalListener.java
+++ org.openwms/trunk/core/org.openwms.core.service.api/src/main/java/org/openwms/core/service/listener/OnRemovalListener.java
@@ -18,7 +18,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.openwms.core.service;
+package org.openwms.core.service.listener;
 
 import org.openwms.core.domain.AbstractEntity;
 import org.openwms.core.service.exception.RemovalNotAllowedException;
