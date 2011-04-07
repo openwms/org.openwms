9ea900b88977301eb72ea9cad588dad8a36b5d7b
scherrer <scherrer@fda5f131-174c-0410-8613-e0905860106b>
2011-04-07 19:00:22 +0000

    Added test
    git-svn-id: https://openwms2005.svn.sourceforge.net/svnroot/openwms2005@1255 fda5f131-174c-0410-8613-e0905860106b

---
+++ org.openwms/trunk/org.openwms.core/org.openwms.core.domain/src/test/java/org/openwms/core/domain/system/MessageTest.java
@@ -0,0 +1,45 @@
+/*
+ * openwms.org, the Open Warehouse Management System.
+ *
+ * This file is part of openwms.org.
+ *
+ * openwms.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as 
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * openwms.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software. If not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.openwms.core.domain.system;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.openwms.core.test.AbstractJpaSpringContextTests;
+
+/**
+ * A MessageTest.
+ * 
+ * @author <a href="mailto:scherrer@users.sourceforge.net">Heiko Scherrer</a>
+ * @version $Revision: $
+ */
+public class MessageTest extends AbstractJpaSpringContextTests {
+
+    @Test
+    public final void testConstruction() {
+        Message m = new Message(4711, "Test message");
+        assertNotNull("Creation date must be set", m.getCreated());
+        assertEquals("Expected the message", "Test message", m.getMessageText());
+        assertEquals("Expected a message number", 4711, m.getMessageNo());
+    }
+
+}
