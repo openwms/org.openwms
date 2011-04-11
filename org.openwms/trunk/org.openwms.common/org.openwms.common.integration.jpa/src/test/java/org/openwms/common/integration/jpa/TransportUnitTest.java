d6eedd5c8d8d401988f67987f3bd7db6e0db7296
Heiko Scherrer <scherrer@openwms.org>
2011-04-11 22:13:51 +0000

    Fixed without component scanning
    git-svn-id: https://openwms2005.svn.sourceforge.net/svnroot/openwms2005@1280 fda5f131-174c-0410-8613-e0905860106b

--- org.openwms/trunk/org.openwms.common/org.openwms.common.integration.jpa/src/test/java/org/openwms/common/integration/jpa/TransportUnitTest.java
+++ org.openwms/trunk/org.openwms.common/org.openwms.common.integration.jpa/src/test/java/org/openwms/common/integration/jpa/TransportUnitTest.java
@@ -18,7 +18,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.openwms.common.integration.jpa.system;
+package org.openwms.common.integration.jpa;
 
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.fail;
