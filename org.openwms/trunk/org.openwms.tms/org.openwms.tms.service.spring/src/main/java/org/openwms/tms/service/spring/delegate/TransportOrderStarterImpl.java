ad977327e2c267ffc9568c903247ffcd496a5a13
scherrer <scherrer@fda5f131-174c-0410-8613-e0905860106b>
2011-05-05 16:44:52 +0000

    moved package
    git-svn-id: https://openwms2005.svn.sourceforge.net/svnroot/openwms2005@1330 fda5f131-174c-0410-8613-e0905860106b

--- org.openwms/trunk/org.openwms.tms/org.openwms.tms.service.spring/src/main/java/org/openwms/tms/service/spring/delegate/TransportOrderStarterImpl.java
+++ org.openwms/trunk/org.openwms.tms/org.openwms.tms.service.spring/src/main/java/org/openwms/tms/service/spring/delegate/TransportOrderStarterImpl.java
@@ -18,7 +18,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.openwms.tms.service.order.delegate;
+package org.openwms.tms.service.spring.delegate;
 
 import java.util.List;
 
