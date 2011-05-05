a80852f025ee907b20dffa39aaf19854c3c903cf
scherrer <scherrer@fda5f131-174c-0410-8613-e0905860106b>
2011-05-05 16:42:45 +0000

    moved package
    git-svn-id: https://openwms2005.svn.sourceforge.net/svnroot/openwms2005@1329 fda5f131-174c-0410-8613-e0905860106b

--- org.openwms/trunk/org.openwms.tms/org.openwms.tms.service.spring/src/main/java/org/openwms/tms/service/spring/TargetAcceptedVoter.java
+++ org.openwms/trunk/org.openwms.tms/org.openwms.tms.service.spring/src/main/java/org/openwms/tms/service/spring/TargetAcceptedVoter.java
@@ -18,7 +18,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.openwms.tms.service.impl;
+package org.openwms.tms.service.spring;
 
 import org.openwms.core.service.voter.DecisionVoter;
 import org.openwms.core.service.voter.DeniedException;
