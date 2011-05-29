2b8e0f70ae21c4c61ac039c4e6c3527d57059a95
scherrer <scherrer@fda5f131-174c-0410-8613-e0905860106b>
2011-05-29 19:36:22 +0000

    Send an event after changing security rules
    git-svn-id: https://openwms2005.svn.sourceforge.net/svnroot/openwms2005@1412 fda5f131-174c-0410-8613-e0905860106b

--- org.openwms/trunk/org.openwms.core/org.openwms.core.service.spring/src/main/java/org/openwms/core/service/spring/SecurityServiceImpl.java
+++ org.openwms/trunk/org.openwms.core/org.openwms.core.service.spring/src/main/java/org/openwms/core/service/spring/SecurityServiceImpl.java
@@ -23,15 +23,14 @@ package org.openwms.core.service.spring;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.openwms.core.domain.system.usermanagement.Grant;
-import org.openwms.core.integration.SecurityObjectDao;
-import org.openwms.core.service.SecurityService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.openwms.core.annotation.FireAfterTransaction;
+import org.openwms.core.util.event.UserChangedEvent;
 
 /**
  * A SecurityServiceImpl.
@@ -54,6 +53,7 @@ public class SecurityServiceImpl implements SecurityService {
      *      java.util.List)
      */
     @Override
+    @FireAfterTransaction(events = { UserChangedEvent.class })
     public List<Grant> mergeGrants(String moduleName, List<Grant> grants) {
         if (logger.isDebugEnabled()) {
             logger.debug("Merging grants of module:" + moduleName);
