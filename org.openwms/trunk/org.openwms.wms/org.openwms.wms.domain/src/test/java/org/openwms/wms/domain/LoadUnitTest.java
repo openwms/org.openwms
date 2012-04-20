bef63287813700500c9c501004828b62d14ae663
scherrer <scherrer@fda5f131-174c-0410-8613-e0905860106b>
2012-04-20 17:32:45 +0000

    3519299 worked on WMS domain model
    git-svn-id: https://openwms2005.svn.sourceforge.net/svnroot/openwms2005@1686 fda5f131-174c-0410-8613-e0905860106b

--- org.openwms/trunk/org.openwms.wms/org.openwms.wms.domain/src/test/java/org/openwms/wms/domain/LoadUnitTest.java
+++ org.openwms/trunk/org.openwms.wms/org.openwms.wms.domain/src/test/java/org/openwms/wms/domain/LoadUnitTest.java
@@ -20,19 +20,48 @@
  */
 package org.openwms.wms.domain;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.openwms.common.domain.Location;
+import org.openwms.common.domain.LocationPK;
 import org.openwms.common.domain.TransportUnit;
+import org.openwms.common.domain.TransportUnitType;
 import org.openwms.core.domain.values.Piece;
 import org.openwms.core.exception.DomainModelRuntimeException;
+import org.openwms.core.test.AbstractJpaSpringContextTests;
 
 /**
  * A LoadUnitTest.
  * 
  * @author <a href="mailto:scherrer@openwms.org">Heiko Scherrer</a>
  * @version $Revision: $
- * 
+ * @since 0.1
  */
-public class LoadUnitTest {
+public class LoadUnitTest extends AbstractJpaSpringContextTests {
+
+    private TransportUnitType tut;
+    private TransportUnit tu;
+    private Location loc1;
+    private Location loc2;
+
+    /**
+     * Setup some test data.
+     */
+    @Before
+    public void onBefore() {
+        tut = new TransportUnitType("TUT");
+        loc1 = new Location(new LocationPK("AREA", "AISLE", "X", "Y", "Z"));
+        loc2 = new Location(new LocationPK("AREA2", "AISLE2", "X2", "Y2", "Z2"));
+        entityManager.persist(tut);
+        entityManager.persist(loc1);
+        entityManager.persist(loc2);
+
+        tu = new TransportUnit("TEST");
+        tu.setTransportUnitType(tut);
+        tu.setActualLocation(loc1);
+        entityManager.persist(tu);
+        entityManager.flush();
+    }
 
     /**
      * Test method for
@@ -41,7 +70,33 @@ public class LoadUnitTest {
      */
     @Test(expected = DomainModelRuntimeException.class)
     public final void testLoadUnit() {
-        LoadUnit loadUnit = new LoadUnit(new TransportUnit("TEST"), "0", new Piece(0));
-        loadUnit.prePersist();
+        LoadUnit loadUnit = new LoadUnit(new TransportUnit("TEST"), "LEFT", new Piece(0));
+        entityManager.persist(loadUnit);
+    }
+
+    /**
+     * Test method for
+     * {@link org.openwms.wms.domain.LoadUnit#LoadUnit(java.lang.String, org.openwms.common.domain.values.Weight)}
+     * .
+     */
+    @Test
+    public final void testLoadUnitOk() {
+        LoadUnit loadUnit = new LoadUnit(new TransportUnit("TEST"), "LEFT", new Piece(100));
+        entityManager.persist(loadUnit);
+    }
+
+    /**
+     * Test method for
+     * {@link org.openwms.wms.domain.LoadUnit#LoadUnit(java.lang.String, org.openwms.common.domain.values.Weight)}
+     * .
+     */
+    @Test
+    public final void testDuplicatedLoadUnit() {
+
+        LoadUnit loadUnit = new LoadUnit(tu, "LEFT", new Piece(100));
+        entityManager.persist(loadUnit);
+        entityManager.flush();
+        LoadUnit loadUnit2 = new LoadUnit(tu, "LEFT", new Piece(20));
+        entityManager.persist(loadUnit2);
     }
 }
