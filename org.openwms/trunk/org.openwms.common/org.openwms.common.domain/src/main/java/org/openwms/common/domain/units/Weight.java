fad603689a3fc2e26d2f924dfa0369d49d5ded85
scherrer <scherrer@fda5f131-174c-0410-8613-e0905860106b>
2012-04-30 15:25:06 +0000

    Moved classes
    git-svn-id: https://openwms2005.svn.sourceforge.net/svnroot/openwms2005@1714 fda5f131-174c-0410-8613-e0905860106b

--- org.openwms/trunk/org.openwms.common/org.openwms.common.domain/src/main/java/org/openwms/common/domain/units/Weight.java
+++ org.openwms/trunk/org.openwms.common/org.openwms.common.domain/src/main/java/org/openwms/common/domain/units/Weight.java
@@ -18,7 +18,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.openwms.common.domain.values;
+package org.openwms.common.domain.units;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
