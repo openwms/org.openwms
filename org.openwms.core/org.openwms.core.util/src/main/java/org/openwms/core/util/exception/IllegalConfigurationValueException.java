3817feec76c0a3dd4539a21583de2c12e6f34a0e
Heiko Scherrer <openwms@gmail.com>
2013-04-17 08:57:34 +0200

    be backward compatible to 1.6

--- org.openwms.core/org.openwms.core.util/src/main/java/org/openwms/core/util/exception/IllegalConfigurationValueException.java
+++ org.openwms.core/org.openwms.core.util/src/main/java/org/openwms/core/util/exception/IllegalConfigurationValueException.java
@@ -77,23 +77,4 @@ public class IllegalConfigurationValueException extends RuntimeException {
     public IllegalConfigurationValueException(String message, Throwable cause) {
         super(message, cause);
     }
-
-    /**
-     * Create a new IllegalConfigurationValueException.
-     * 
-     * @param message
-     *            the detail message. The detail message is saved for later
-     *            retrieval by the {@link #getMessage()} method.
-     * @param cause
-     *            the cause (which is saved for later retrieval by the
-     *            {@link #getCause()} method). (A <tt>null</tt> value is
-     *            permitted, and indicates that the cause is nonexistent or
-     *            unknown.)
-     * @param enableSuppression
-     * @param writableStackTrace
-     */
-    public IllegalConfigurationValueException(String message, Throwable cause, boolean enableSuppression,
-            boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-    }
 }
