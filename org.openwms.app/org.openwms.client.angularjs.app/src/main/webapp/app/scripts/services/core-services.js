014914f4d3649d2911378ed24f0ad0dc22cd7f47
Heiko Scherrer <scherrer@openwms.org>
2014-02-07 00:04:26 +0100

    AJS-2 worked on roles management

--- org.openwms.app/org.openwms.client.angularjs.app/src/main/webapp/app/scripts/services/core-services.js
+++ org.openwms.app/org.openwms.client.angularjs.app/src/main/webapp/app/scripts/services/core-services.js
@@ -35,20 +35,21 @@ servicesModule.factory('rolesService',['$http', '$resource', '$q', 'toaster',
 	function($http, $resource, $q, toaster) {
 		return {
 			/**
-			 * Create a non-existing Role instance and send an POST request to the backend. If the Role with its unique identifier exists the service will reject the request to the caller.
+			 * Create a non-existing Role instance and send an POST request to the backend. If the Role with its unique identifier exists the service will reject the request.
+			 *
 			 * @param $scope The current scope
 			 * @param role the Role to add
-			 * @returns {Promise.promise|*} A promise to evaluate
+			 * @returns {Promise.promise|*} A promise to evaluate: In case of success the Role is returned, otherwise the ResponseItem
 			 */
 			add : function($scope, role) {
 				var delay = $q.defer();
 				$http.defaults.headers.put['Auth-Token'] = $scope.authToken;
 				$http.post($scope.rootUrl+'/roles', role)
-					.success(function (addedRole) {
-						delay.resolve(addedRole);
+					.success(function (data) {
+						delay.resolve(data.items[0].obj[0]);
 					})
-					.error(function (addedRole) {
-						delay.reject(addedRole);
+					.error(function (data) {
+						delay.reject(data);
 					});
 				return delay.promise;
 			},
@@ -57,7 +58,7 @@ servicesModule.factory('rolesService',['$http', '$resource', '$q', 'toaster',
 			 *
 			 * @param $scope The current scope
 			 * @param roles The Roles to remove, at least the rolename has to be set
-			 * @returns {Promise.promise|*} A promise to evaluate
+			 * @returns {Promise.promise|*} A promise to evaluate: In case of success nothing is returned, otherwise the ResponseItem
 			 */
 			delete : function($scope, roles) {
 				var param = "";
@@ -68,43 +69,44 @@ servicesModule.factory('rolesService',['$http', '$resource', '$q', 'toaster',
 				$http.defaults.headers.put['Auth-Token'] = $scope.authToken;
 				$http.delete($scope.rootUrl+'/roles/'+ param)
 					.success(function () {
-						delay.resolve(roles);
+						delay.resolve();
 					})
 					.error(function (data) {
 						delay.reject(data);
 					});
 				return delay.promise;
 			},
+			/**
+			 * Send a http PUT request to save a Role. The Role to save must already exist.
+			 *
+			 * @param $scope The current scope
+			 * @param role The existing Role to update
+			 * @returns {Promise.promise|*} A promise to evaluate: In case of success the updated Role is returned, otherwise the ResponseItem
+			 */
 			save : function ($scope, role) {
 				var delay = $q.defer();
 				$http.defaults.headers.put['Auth-Token'] = $scope.authToken;
 				$http.put($scope.rootUrl+'/roles', role)
 					.success(function (savedRole) {
-						delay.resolve(roles);
-						/*
-						angular.forEach($scope.roleEntities, function (role) {
-							if (role.name == savedRole.name) {
-								role = savedRole;
-							}
-						});
-						*/
+						delay.resolve(savedRole);
 					})
-					.error(function (data, status) {
+					.error(function (data) {
 						delay.reject(data);
-						/*
-						var msg = "Error ["+status+"] while saving a Role: ["+role.name+"]/["+role.description+"]";
-						console.log(msg);
-						throw new Error(msg);
-						*/
 					});
 				return delay.promise;
 			},
+			/**
+			 * Send a http GET request to load and retrieve all existing Roles from the backend.
+			 *
+			 * @param $scope The current scope
+			 * @returns {Promise.promise|*} A promise to evaluate: In case of success all retrieved Roles are returned, otherwise the ResponseItem
+			 */
 			getAll : function($scope) {
 				var delay = $q.defer();
 				$http.defaults.headers.common['Auth-Token'] = $scope.authToken;
 				$http.get($scope.rootUrl+'/roles')
 					.success(function (data) {
-						delay.resolve(data);
+						delay.resolve(data.items[0].obj[0]);
 					})
 					.error(function (data) {
 						delay.reject(data);
