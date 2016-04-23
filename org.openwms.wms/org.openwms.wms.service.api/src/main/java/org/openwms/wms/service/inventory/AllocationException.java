527cad9e1a3923d89c813f365af085bd1e9aba56
Heiko Scherrer <scherrer@openwms.org>
2016-04-23 23:31:24 +0200

    WMS: Moved first sketch of the service API from Bitbucket to GH. Without history

---
+++ org.openwms.wms/org.openwms.wms.service.api/src/main/java/org/openwms/wms/service/inventory/AllocationException.java
@@ -0,0 +1,66 @@
+/*
+ * openwms.org, the Open Warehouse Management System.
+ * Copyright (C) 2014 Heiko Scherrer
+ *
+ * This file is part of openwms.org.
+ *
+ * openwms.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * openwms.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software. If not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.openwms.wms.service.inventory;
+
+import org.openwms.core.service.exception.ServiceException;
+
+/**
+ * A AllocationException.
+ *
+ * @author <a href="mailto:scherrer@openwms.org">Heiko Scherrer</a>
+ * @version $Revision: $
+ *
+ */
+public class AllocationException extends ServiceException {
+
+    /**
+     * Create a new AllocationException.
+     * 
+     * @param message
+     */
+    public AllocationException(String message) {
+        super(message);
+        // TODO [scherrer] Auto-generated constructor stub
+    }
+
+    /**
+     * Create a new AllocationException.
+     * 
+     * @param cause
+     */
+    public AllocationException(Throwable cause) {
+        super(cause);
+        // TODO [scherrer] Auto-generated constructor stub
+    }
+
+    /**
+     * Create a new AllocationException.
+     * 
+     * @param message
+     * @param cause
+     */
+    public AllocationException(String message, Throwable cause) {
+        super(message, cause);
+        // TODO [scherrer] Auto-generated constructor stub
+    }
+
+}
