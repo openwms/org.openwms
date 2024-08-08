5143c530505d1f490661b9d3d4d820e7e8cf8ce3
Heiko Scherrer <scherrer@openwms.org>
2024-08-08 18:01:15 +0200

    #180 Introduced Triple record to combine key/value/type

---
+++ org.openwms.core.util/src/main/java/org/openwms/core/lang/Triple.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2005-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openwms.core.lang;
+
+/**
+ * A Triple.
+ *
+ * @author Heiko Scherrer
+ */
+public record Triple<K, V, T>(K key, V value, T type) {
+
+    /**
+     * Get the stored value as class type {@code clazz}.
+     *
+     * @param clazz The type to retrieve the value as
+     * @return The cast value
+     * @param <U> The requested type
+     * @throws ClassCastException in case of cast error
+     */
+    @SuppressWarnings("unchecked")
+    public <U> U valueAs(Class<U> clazz) {
+        if (clazz.equals(this.type.getClass())) {
+            return (U) value;
+        }
+        throw new ClassCastException("Cannot case requested type [%s] from [%s]".formatted(clazz, type.getClass()));
+    }
+}
